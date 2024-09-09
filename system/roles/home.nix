1688415bca69ed3907feaef31b4614580db9b1fd
Falkgaard <falkgaard@gmail.com>
2024-09-09 10:22:06 +0200

    Hyprland integration WIP

--- system/roles/home.nix
+++ system/roles/home.nix
@@ -5,7 +5,7 @@
      #../profiles/boot/quiet.nix (Not working out as I had hoped...)
       ../profiles/boot/grub.nix
      #../profiles/boot/plymouth.nix (Kinda disappointing.)
-      ../profiles/desktop/kde-plasma6.nix
+     #../profiles/desktop/kde-plasma6.nix
       ../profiles/desktop/hyprland.nix
       ../profiles/general/audio.nix
    ];
